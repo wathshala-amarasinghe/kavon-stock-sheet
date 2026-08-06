aa675cb95a6d40c6783ae418e69b429df25f3aee
Wathshala Amarasinghe <wathshaladulashan@outlook.com>
2026-08-06 16:01:27 +0530

    Initial KAVON stock sheet application

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { ignores: [".next/", "node_modules/", "tailwind.config.ts"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
